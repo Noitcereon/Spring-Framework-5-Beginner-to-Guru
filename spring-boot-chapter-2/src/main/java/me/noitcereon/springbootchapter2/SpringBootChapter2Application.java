9c8ac972570280b04f4b91d64fa937e8cdc61ee3
Noitcereon <tba@live.dk>
2024-08-09 10:03:16 +0200

    Add Spring Initializr template with a few modifications
    Removes some empty metadata tags due to unused, downgraded to
    Spring Boot 2.2.2-RELEASE (instead of v3.x.y).
    This is so I can follow
    along with the course easier and because I work with legacy
    code anyway.

---
+++ spring-boot-chapter-2/src/main/java/me/noitcereon/springbootchapter2/SpringBootChapter2Application.java
@@ -0,0 +1,13 @@
+package me.noitcereon.springbootchapter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChapter2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootChapter2Application.class, args);
+    }
+
+}
---
+++ spring-boot-chapter-2/src/test/java/me/noitcereon/springbootchapter2/SpringBootChapter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.noitcereon.springbootchapter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChapter2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
